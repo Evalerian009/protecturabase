2a20ebc2e92da9e311fd43c87836cf51806eb319
unknown <you@example.com>
2025-02-15 23:15:25 +0100

    first commit

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -24,7 +24,7 @@ const Reviews = () => {
             <div key={slide.id} className="px-2.5">
               <div className="w-fit mx-auto text-text border border-border py-10 px-7 rounded-3xl">
                 <IoChatbubblesSharp className="mx-auto text-6xl text-grayTxt"/>
-                <p className="md:w-[83%] lg:w-[88%] mx-auto text-[16px] h-[150px] mt-7 text-grayTxt">{slide.text}</p>
+                <p className="md:w-[83%] lg:w-[88%] mx-auto text-[16px] h-[225px] md:h-[150px] mt-7 text-grayTxt">{slide.text}</p>
                 <HiUser className="mx-auto text-[2.25rem] rounded-full text-grayTxt" />
                 <h5 className="mt-0.5 text-tet text-center text-mutedTxt">{slide.user}</h5>
               </div>
